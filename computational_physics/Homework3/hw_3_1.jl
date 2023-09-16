0d28cdc520882be9016387abe30e3544ddd0ca63
arnab82 <arnabbachhar39@gmail.com>
2023-09-16 16:24:56 -0400

    homework 3 added

---
+++ computational_physics/Homework3/hw_3_1.jl
@@ -0,0 +1,57 @@
+using Printf
+using LinearAlgebra
+function generalised_partial_pivoting(A::Matrix, b::Vector)
+    n = size(A)[1]
+    for col in 1:n
+        # Finding the row with the maximum value in the current column(this will search each column but only for lower triangular matrix i.e below the the diagonal elements)
+        max_row = argmax(abs.(A[col:end, col])) + col - 1
+        
+        if max_row != col
+            # Swap rows in A, b
+            A[[col, max_row], :] = A[[max_row, col], :]
+            b[[col, max_row]] = b[[max_row, col]]
+        end
+    end
+    
+    return A, b
+end
+
+
+function gauss_elimination(A::Matrix, b::Vector)
+    n = size(A)[1]
+    
+    # Forward elimination
+    for i in 1:n#columnwise
+        for j in i+1:n#rowwise
+            A, b= generalised_partial_pivoting(A, b)
+            factor = A[j, i] / A[i, i]
+            A[j, i:end] .-= factor * A[i, i:end]
+            b[j] -= factor * b[i]
+            # display(A)
+            display(hcat(A,b))
+        end
+    end
+    
+    # Backward substitution
+    x = zeros(n)
+    for i in n:-1:1#We are moving from the last row to upward direction
+        x[i] = (b[i] - dot(A[i, i+1:end], x[i+1:end])) / A[i, i]
+    end
+    
+    return x
+end
+
+# Given matrix A and right-hand side vector b
+A = [1.0 -1.0 -2.0 1.0;
+     3.0 2.0 -1.0 2.0;
+     2.0 3.0 1.0 -3.0;
+     10.0 -4.0 3.0 2.0]
+
+b = [1.0, 4.0, 2.0, 3.0]
+
+# Solving the system of equations via gauss elimination
+x = gauss_elimination(A, b)
+expected_solution = [21/34, 43/68, -13/34, 1/4]
+
+@printf("Evaluated Solution (x, y, z, u): %6.4f %6.4f %6.4f %6.4f\n", x[1], x[2], x[3], x[4])
+@printf("Expected Solution (x, y, z, u): %6.4f %6.4f %6.4f %6.4f\n", expected_solution[1], expected_solution[2], expected_solution[3], expected_solution[4])
---
+++ computational_physics/Homework3/hw_3_2.jl
@@ -0,0 +1,82 @@
+using LinearAlgebra
+using Printf
+function lu_decomposition_pivoting(A::Matrix)
+    n = size(A, 1)
+    L = Matrix{Float64}(I, n, n)#initializing Lower traingular matrix as an identity matrix
+    U = copy(A)                 #initializing upper traingular matrix as a copy of the original matrix
+    P = Matrix{Float64}(I, n, n)#initializing permutation matrix as an identity matrix
+
+    for k in 1:n-1
+        pivot_row = argmax(abs.(U[k:end, k])) + k - 1#finding the row index with the maximum absolute value in the current column k for
+                                                    # U matrix and adding the k-1 for giving the actual index in the original U matrix as argmax considers k as the first row.
+
+        if pivot_row != k
+            # Swapping rows in U and apply the same permutation to P
+            U[[k, pivot_row], :] = U[[pivot_row, k], :]
+            P[[k, pivot_row], :] = P[[pivot_row, k], :]
+
+            if k > 1
+                # Appling the same permutation to L for rows below the first row as it contains only one non-zero element(1) in the diagonal
+                L[[k, pivot_row], 1:k-1] = L[[pivot_row, k], 1:k-1]
+            end
+        end
+
+        for i in k+1:n
+            factor = U[i, k] / U[k, k]
+            L[i, k] = factor
+            U[i, k:end] .-= factor * U[k, k:end]
+        end
+    end
+
+    return L, U, P
+end
+
+
+function lu_decomposition(A,lu_decomposition_pivoting::Function, b::Vector)
+    L,U,P=lu_decomposition_pivoting(A)
+    # Solve Ly = Pb
+    y = L \ (P * b)
+
+    # Solve Ux = y
+    x = U \ y
+    return L,U,P,x
+end
+
+# Given matrix A and right-hand side vector b
+A = [1.0 -1.0 -2.0 1.0;
+     3.0 2.0 -1.0 2.0;
+     2.0 3.0 1.0 -3.0;
+     10.0 -4.0 3.0 2.0]
+
+b = [1.0, 4.0, 2.0, 3.0]
+# Getting the L,U,P and solution vector
+L,U,P,x= lu_decomposition(A,lu_decomposition_pivoting,b)
+# Display L, U, and P matrices
+println("L Matrix:")
+display(L)
+println("U Matrix:")
+display(U)
+println("P Matrix:")
+display(P)
+# Checking if L·U = P·A
+PA = P * A
+LU = L * U
+
+println("LU Matrix:")
+display(LU)
+println("PA Matrix:")
+display(PA)
+println("Checking if L·U = P·A")
+println("L·U == P·A:\n", isapprox(LU, PA))
+
+# Display the solution vector
+println("Solution vector")
+@printf(" x\t y\t  z\t u\n%6.4f %6.4f %6.4f %6.4f\n", x[1], x[2], x[3], x[4])
+
+println("Check if the numerical solution satisfies the given answer")
+given_answer = A \ b
+println("Numerical solution matches the given answer:", isapprox(x, given_answer))
+
+# Computing the determinant of matrix A
+det_A = det(A)
+println("Determinant of matrix A:", det_A)
